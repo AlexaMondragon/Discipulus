0451f4700fdd238478aea96229dd3fb82e93c872
Valerie Mondragon <mondragonva7@gmail.com>
2023-06-12 12:26:54 -0700

    Initial Commit

---
+++ Discipulus/DiscipulusApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiscipulusApp.swift
+//  Discipulus
+//
+//  Created by Patricia Mondragon on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct DiscipulusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
